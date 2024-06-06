e81868130829c56f5defa8569070e5a2b479d797
huyme.27 <141383639+daicahuy@users.noreply.github.com>
2024-06-06 09:53:10 +0700

    CRUD tags  (#14)
    * load
    
    * load
    
    ---------
    
    Co-authored-by: tuanhiep1405 <tuanhiep240604@gmail.com>

--- src/Views/Admin/pages/tags/edit.blade.php
+++ src/Views/Admin/pages/tags/edit.blade.php
@@ -20,22 +20,27 @@
                         <div class="card">
                             <div class="card-body">
                                 <h2 class="mb-4">
+                                    
                                     <span class="badge badge-default"> Edit Tag </span>
                                 </h2>
+                                @include('components.alert')
+
                                 <form action="" method="POST" enctype="multipart/form-data">
                                     <div class="form-group">
                                         <label class="d-block">ID</label>
-                                        <input class="form-control" type="text" name="id" disabled>
+                                        <input class="form-control" type="text" name="id" value="{{ $tags['id'] }}" disabled>
                                     </div>
                                     <div class="form-group">
                                         <label class="d-block">Name</label>
-                                        <input class="form-control" type="text" placeholder="Name Tag..." name="name">
+                                        <input class="form-control" type="text" placeholder="Name Tag..." name="nameTag" value="{{ $tags['nameTag'] }}">
                                     </div>
+
+
                                     {{-- <div class="form-group">
                                         <label class="d-block">Image</label>
                                         <input type="file" name="image" accept="image/*">
                                     </div> --}}
-                                    <button type="submit" class="btn btn-warning waves-effect waves-light float-right">
+                                    <button name="btn-edit" type="submit" class="btn btn-warning waves-effect waves-light float-right">
                                         Edit
                                     </button>
                                 </form>
